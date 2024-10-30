37ce1cf603481575db850d6b87c87e1c7683dbcc
SavionShipp <shippsavion209@yahoo.com>
2024-10-29 19:06:08 -0700

    added a index action to see all workouts

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
-
+import db
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/workouts')
+def index():
+    return db.workouts_all()
